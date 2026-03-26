8fc7737e74e421c39d2a7c9bebc14603e88da1b6
Matheus Viana <196691829+matviana-create@users.noreply.github.com>
2026-03-25 22:03:57 -0300

    fix: force-dynamic para evitar cache do vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { headers } from 'next/headers'
 import { createClient } from '@supabase/supabase-js'
 import PaginaPedidosClient from './_components/PaginaPedidosClient'
